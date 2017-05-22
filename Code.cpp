297d7b3c4ac8820906a910cbaadbe4473186c223
Ayush-Mahajan <mail4ayushmahajan@gmail.com>
2017-05-23 00:15:56 +0530

    Create Code.cpp

---
+++ Code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+http://www.dreamincode.net/
+int main()
+{
+    int i, j;
+    car i1,i2,i3,i4,i5,i6,i7,i8;
+    car group [4][8];
+    char c_team[20][1]={{"Barcelona",i1},{"Bayern",i2},{"Benfica",i3},{"Chelsea",i4},{"Juventus",i5},{"Paris",i6},{"PSV",i7},{"Zenit",i8}};
+    char team[24][2]={{"Arsenal",i4,"1"},{"ManC",i4,"2"},{"ManU",i4,"3"},{"Astana","100","1"},{"Atletico",i1,"1"},{"Real Madrid",i1,"2"},{"Sevilla",i1,"3"},{"Valencia",i1,"4"},{"Leverkusen",i2,"1"},{"Monchengladbch",i2,"2"},{"Olympiacos",i2,"3"},{"Wolfsburg",i2,"4"},{"Porto",i3,"1"},{"Roma",i5,"1"},{"Lyon",i6,"1"},{"CSKA Moskva",i8,"1"},{"BATE","101","1"},{"Dinamo Zagerb","102","1"},{"Dynamo Kyiv","103","1"},{"Shakhtar Donetsk","103","2"},{"Galatasaray","104","1"},{"Gent","105","1"},{"M.Tel-Aviv","106","1"},{"Malmo","107","1"}};
+    char ESP[20][4]={}
+    cout << "Hello world!" << endl;
+    for(i=1 ; i<=8 ; i++)
+    return 0;
+}
